2858952297373540e5fdca1583364e8ae88b461d
Denali Balser <denali.balser167@gmail.com>
2019-05-17 11:53:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,8 +17,14 @@ def summon_captain_planet(array)
   end
 
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.each do |element|
+    if element > 4
+      false
+    else 
+      true
+    end
+  end
 end
 
 def find_the_cheese# code an argument here
