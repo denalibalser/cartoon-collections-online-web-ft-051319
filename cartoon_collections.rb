504ea9f31dc6e671426727740f7dcb35494206b6
Denali Balser <denali.balser167@gmail.com>
2019-05-17 12:01:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,9 +21,9 @@ def long_planeteer_calls(array)
   array.each do |element|
     #binding.pry
     if element.length > 4
-      return false
-    else
       return true
+    else
+      return false
     end
   end
 end
