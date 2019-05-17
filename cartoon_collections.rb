c16547fe4694f40cea236d66e6b304be6a69e31b
Denali Balser <denali.balser167@gmail.com>
2019-05-17 12:05:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,16 +17,15 @@ def summon_captain_planet(array)
   end
 
 
-def long_planeteer_calls(array)
-  array.each do |element|
-    #binding.pry
-    if element.length.any? > 4
+  def long_planeteer_calls(array)
+    i = 0
+    if  array.any? {|i| i.length > 4}
       return true
     else
       return false
+    i = i + 1
     end
   end
-end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
